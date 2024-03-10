77bb304a1a59bd50677ae1f1c8765c1e839aafd9
regina <usmanova.re@yandex.ru>
2024-03-10 23:30:33 +0500

    fixup! fixup! Decor
    Добавила кнопки для перехода по активностям

--- app/src/main/java/com/example/profileroom/MainActivity.kt
+++ app/src/main/java/com/example/profileroom/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.profileroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonCreate =findViewById<Button>(R.id.buttonCreate)
+        buttonCreate.setOnClickListener {
+            val intent = Intent(this, EditProfile::class.java)
+            startActivity(intent)
+        }
     }
 }
