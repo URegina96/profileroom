4047542a5975f04cbd5aaf5c89e55c2849e579e7
regina <usmanova.re@yandex.ru>
2024-03-15 16:46:04 +0500

    Для отображения данных из базы данных Room в активности MainActivity необходимо выполнить запрос к базе данных и получить список всех пользователей
    ➤ В активности MainActivity добавь код для получения списка всех пользователей из базы данных и отображения их на экране  в соответствующих TextView
    
    ✓  Что бы не было ошибок в логах (а имено при запуске приложение сразу закрывается) - это ошибка, получаемая, связана с тем, что попытка доступа к базе данных происходит на главном потоке выполнения, что может заблокировать пользовательский интерфейс на длительное время
    Для исправления этой ошибки необходимо использовать фоновый поток для доступа к базе данных. В этом случае можно использовать библиотеку Kotlin Coroutines для выполнения этой задачи
     Нужно обернуть блок кода, который получает список пользователей из базы данных, в корутины
    Теперь запрос к базе данных будет выполняться в фоновом потоке, и приложение не будет блокироваться

--- app/src/main/java/com/example/profileroom/MainActivity.kt
+++ app/src/main/java/com/example/profileroom/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.room.Room
 import com.example.profileroom.database.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var db: AppDatabase
@@ -17,6 +21,19 @@ class MainActivity : AppCompatActivity() {
             applicationContext,
             AppDatabase::class.java, "app-database"
         ).build()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val users = db.userDao().getAll() // Получить список всех пользователей из базы данных
+
+            for (user in users) {
+                val textViewName = findViewById<TextView>(R.id.textViewName)
+                textViewName.text = user.name
+                val textViewAge = findViewById<TextView>(R.id.textViewAge)
+                textViewAge.text = user.age.toString()
+                val textViewEmail = findViewById<TextView>(R.id.textViewEmail)
+                textViewEmail.text = user.email
+            }
+        }
 
         val buttonCreate = findViewById<Button>(R.id.buttonCreate)
         buttonCreate.setOnClickListener {
